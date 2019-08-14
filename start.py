bf8e7ee167f978470e25e77787aefc633ec7821a
sowmiyavelu17 <53555335+sowmiyavelu17@users.noreply.github.com>
2019-08-14 14:16:45 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,4 @@
+print("using start, stop, and step arguments in Python range()function")
+print("Printing All odd numbers between 1 and 10 using range()")
+for i inrange(1, 10, 2):
+    print(i, end=', ')
