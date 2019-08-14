8a69e28f406a3c3526badd907f37608ad01b10fb
sowmiyavelu17 <53555335+sowmiyavelu17@users.noreply.github.com>
2019-08-14 14:52:42 +0530

    Create cap.py

---
+++ cap.py
@@ -0,0 +1,8 @@
+name = "geeks for geeks"
+  print(name.capitalize())  
+ name1="geeks"
+name2="for"
+name3="geeks"
+print(name1.capitalize() + name2.capitalize() 
+                         + name3.capitalize()) 
+ 
